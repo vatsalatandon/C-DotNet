4db866a119f93b4313c96fcb4d6a1cf63c717076
vatsala.tandon <vatsala.tandon@CYG391>
2020-03-25 11:17:56 +0530

    Section 2 Exercise

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,11 +13,11 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shutter Island" };
+            var movie = new Movie() { Name = "The Mummy" };
             var customers = new List<Customer>
             {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
@@ -28,11 +28,20 @@ namespace Vidly.Controllers
 
             return View(viewModel);
         }
-
-        /*[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
+        // GET: Movies
+        public ActionResult Index()
         {
-            return Content(year + "/" + month);
-        }*/
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shutter Island" },
+                new Movie { Id = 2, Name = "K3G" }
+            };
+        }
     }
 }
