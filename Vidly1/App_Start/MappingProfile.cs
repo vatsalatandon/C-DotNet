00ed5b67af080373a557426c8f0efde5eeb33138
vatsala.tandon <vatsala.tandon@CYG391>
2020-03-30 15:56:40 +0530

    Section 6 Exercise

---
+++ Vidly1/App_Start/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+using Vidly1.Dtos;
+
+namespace Vidly1.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore()); ;
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore()); ;
+        }
+    }
+}
---
+++ Vidly1/Dtos/CustomerDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly1.Dtos
+{
+    public class CustomerDto
+    {
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
